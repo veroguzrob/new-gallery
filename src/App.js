da6ccfd367392762e7966d2a42445b9f51f7a730
veroguzrob72 <vgrdesd@gmail.com>
2023-09-12 18:46:42 -0600

    Learning exercise for a photo gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import WSPGallery from './components/WSPGallery';
 
 function App() {
+
+  const galleryImages = [
+
+    {
+      img: 'https://images.pexels.com/photos/18293981/pexels-photo-18293981/free-photo-of-ligero-ciudad-gente-edificio.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+    },
+    {
+      img: 'https://images.pexels.com/photos/18254681/pexels-photo-18254681.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+    },
+    {
+      img: 'https://images.pexels.com/photos/18306635/pexels-photo-18306635/free-photo-of-contraste.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+    },
+    {
+      img: 'https://images.pexels.com/photos/14611936/pexels-photo-14611936.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+    },
+    {
+      img: 'https://images.pexels.com/photos/18319967/pexels-photo-18319967.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <div>
+        <strong>Responsive Photo Gallery (No External Library) in React JS</strong>
+      </div>
+      <br /><br />
+
+      <WSPGallery
+        galleryImages={galleryImages}
+      />
+
+      <br /><br />
+      <div>- WebStylePress -</div>
     </div>
   );
 }
